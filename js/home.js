3e0d7179767ba3f508beddea9e1234797f479ab2
sawyersieja <sawyersieja@protonmail.com>
2024-04-07 01:14:12 -0700

    Created sponsor banner that alternates between 2 colors every 8 seconds to catch attention.

--- js/home.js
+++ js/home.js
@@ -3,3 +3,28 @@ $(document).ready(function(){
         window.location.href = "../html/home.html";
     });
 });
+//---------------------------------------------------------------------------------
+$(document).ready(function(){
+    $("#sponsor-img").click(function() {
+        alert("cha-ching!");
+    });
+});
+//----------------------------------------------------------------------------------
+var sponsor_img = [
+    "../assets/img/sponsor.png",
+    "../assets/img/sponsor2.png",
+];
+
+var count = 0;
+
+function changeSponsor() {
+    count++;
+    if(count == sponsor_img.length) {
+        count = 0;
+    }
+
+    document.getElementById("sponsor-img").src = sponsor_img[count];
+}
+
+setInterval(changeSponsor, 8000);
+//------------------------------------------------------------------------------------
